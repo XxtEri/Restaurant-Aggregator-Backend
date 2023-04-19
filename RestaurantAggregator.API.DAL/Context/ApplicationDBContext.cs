a7b91a27429921ec0b2ce5529a586d448f83e329
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-04-19 20:44:48 +0700

    Добавлен контекст

---
+++ RestaurantAggregator.API.DAL/Context/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace RestaurantAggregator.API.DAL;
+
+public class ApplicationDBContext: DbContext
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
